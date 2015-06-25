0195f7728ded351ab68859307c74d9c7bcc082b1
Rolf Håvard Blindheim <rhblind@gmail.com>
2015-06-25 12:47:40 +0200

    Fixed CurrentPageCookieMiddleware to only kick in on "cms pages"

--- djangocms_restapi/middleware.py
+++ djangocms_restapi/middleware.py
@@ -8,24 +8,27 @@ from cms.middleware.page import get_page
 
 class CurrentPageCookieMiddleWare(object):
     """
-    Inserts the current page in the session
+    Sets the `current_page` in the Cookie Store.
     """
 
     def process_response(self, request, response):
         current_page = getattr(request, "current_page", get_page(request))
         if current_page:
+            # Only update do this if we're on a "cms page"!
             current_page = current_page.get_absolute_url()
 
-        if hasattr(request, "session"):
-            session_page = request.session.get("current_page", None)
-            if session_page and not session_page == current_page:
-                request.session["current_language"] = current_page
-                request.session.save()
+            if hasattr(request, "session"):
+                session_page = request.session.get("current_page", None)
+                if not session_page == current_page:
+                    request.session["current_language"] = current_page
+                    request.session.save()
 
-        if "current_page" in request.COOKIES and request.COOKIES["current_page"] == current_page:
+            if "current_page" in request.COOKIES and request.COOKIES["current_page"] == current_page:
+                return response
+
+            max_age = 365 * 24 * 60 * 60  # 10 years
+            expires = datetime.utcnow() + timedelta(seconds=max_age)
+            response.set_cookie("current_page", current_page, expires=expires)
             return response
 
-        max_age = 365 * 24 * 60 * 60  # 10 years
-        expires = datetime.utcnow() + timedelta(seconds=max_age)
-        response.set_cookie("current_page", current_page, expires=expires)
         return response
